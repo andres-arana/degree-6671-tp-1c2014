d6304ca63d2e0eb50b439182f96c79d274770c7a
Andres Arana <and2arana@gmail.com>
2014-05-17 01:18:49 -0300

    Implements box and triangular box

--- source/javascripts/geometries.js
+++ source/javascripts/geometries.js
@@ -72,6 +72,22 @@ sg.geometries = sg.geometries || {};
       0);
   };
 
+  function drawWithTriangles(m) {
+    this.context.shaders.basic.setModelMatrix(m);
+
+    var attribute = this.context.shaders.basic.getPositionAttribute();
+
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexBuffer);
+    this.gl.vertexAttribPointer(attribute, 3, this.gl.FLOAT, false, 0, 0);
+
+    this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+    this.gl.drawElements(
+      this.gl.TRIANGLES,
+      this.indexBuffer.items,
+      this.gl.UNSIGNED_SHORT,
+      0);
+  };
+
   sg.geometries.Terrain = function(context, heightmap) {
     var pictureWidth = 200;
     var pictureHeight = 200;
@@ -175,5 +191,93 @@ sg.geometries = sg.geometries || {};
   };
 
   sg.geometries.Cylinder.prototype.draw = drawWithTriangleStrips;
+
+  sg.geometries.Box = function(context) {
+    this.context = context;
+    this.gl = context.gl;
+
+    var vertices = [
+      // Front face
+      -1.0, -1.0,  1.0,
+      1.0, -1.0,  1.0,
+      1.0,  1.0,  1.0,
+      -1.0,  1.0,  1.0,
+
+      // Back face
+      -1.0, -1.0, -1.0,
+      -1.0,  1.0, -1.0,
+      1.0,  1.0, -1.0,
+      1.0, -1.0, -1.0,
+
+      // Top face
+      -1.0,  1.0, -1.0,
+      -1.0,  1.0,  1.0,
+      1.0,  1.0,  1.0,
+      1.0,  1.0, -1.0,
+
+      // Bottom face
+      -1.0, -1.0, -1.0,
+      1.0, -1.0, -1.0,
+      1.0, -1.0,  1.0,
+      -1.0, -1.0,  1.0,
+
+      // Right face
+      1.0, -1.0, -1.0,
+      1.0,  1.0, -1.0,
+      1.0,  1.0,  1.0,
+      1.0, -1.0,  1.0,
+
+      // Left face
+      -1.0, -1.0, -1.0,
+      -1.0, -1.0,  1.0,
+      -1.0,  1.0,  1.0,
+      -1.0,  1.0, -1.0
+    ];
+
+    this.vertexBuffer = buildVertexBuffer(this.gl, vertices);
+
+    var indices = [
+      0, 1, 2,      0, 2, 3,    // Front face
+      4, 5, 6,      4, 6, 7,    // Back face
+      8, 9, 10,     8, 10, 11,  // Top face
+      12, 13, 14,   12, 14, 15, // Bottom face
+      16, 17, 18,   16, 18, 19, // Right face
+      20, 21, 22,   20, 22, 23  // Left face
+    ];
+    this.indexBuffer = buildIndexBuffer(this.gl, indices);
+  };
+
+  sg.geometries.Box.prototype.draw = drawWithTriangles;
+
+  sg.geometries.TriangularBox = function(context) {
+    this.context= context;
+    this.gl = context.gl;
+
+    var vertices = [
+      // Front face
+      1, 1, 1,
+      1, 1, -1,
+      -1, 1, 1,
+      -1, 1, -1,
+
+      // Back face
+      1, -1, 1,
+      -1, -1 ,1
+    ];
+
+    this.vertexBuffer = buildVertexBuffer(this.gl, vertices);
+
+    var indices = [
+      0, 1, 2, 1, 3, 2, // Front face
+      0, 4, 1, // Left face
+      2, 3, 5, // Right face
+      0, 2, 4, 4, 2, 5, // Top face
+      1, 4, 5, 1, 5, 3, // Back face
+    ];
+
+    this.indexBuffer = buildIndexBuffer(this.gl, indices);
+  };
+
+  sg.geometries.TriangularBox.prototype.draw = drawWithTriangles;
 
 })();
