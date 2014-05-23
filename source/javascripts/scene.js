3f45904bccb437036e0fbc487422bfe461b50575
Andres Arana <and2arana@gmail.com>
2014-05-22 21:46:23 -0300

    Add proper train position

--- source/javascripts/scene.js
+++ source/javascripts/scene.js
@@ -4,7 +4,7 @@ sg.Scene = function(context, heightmap) {
   this.context = context;
   this.gl = this.context.gl;
 
-  this.camera = new sg.cameras.Rotating(this.context, vec3.fromValues(0, -30, 0), 55);
+  this.camera = new sg.cameras.Rotating(this.context, vec3.fromValues(0, 0, 0), 55);
   this.terrain = new sg.objects.Terrain(this.context, heightmap);
   this.train = new sg.objects.Train(this.context);
   this.track = new sg.objects.Track(this.context);
@@ -28,11 +28,13 @@ sg.Scene.prototype.draw = function() {
   mat4.translate(trackMatrix, trackMatrix, vec3.fromValues(0, 0, -12));
   this.track.draw(trackMatrix);
 
-  // var trainMatrix = mat4.clone(modelMatrix);
-  // mat4.translate(trainMatrix, trainMatrix, vec3.fromValues(0, 0, -9));
-  // mat4.rotateZ(trainMatrix, trainMatrix, Math.PI / 2);
-  // mat4.scale(trainMatrix, trainMatrix, vec3.fromValues(0.2, 0.2, 0.2));
-  // this.train.draw(trainMatrix);
+  var trainMatrix = mat4.clone(modelMatrix);
+  var trainPosition = this.track.path.evaluate(0);
+  vec3.subtract(trainPosition, trainPosition, vec3.fromValues(0, 0, 9.4));
+  mat4.translate(trainMatrix, trainMatrix, trainPosition);
+  mat4.rotateZ(trainMatrix, trainMatrix, Math.PI / 2);
+  mat4.scale(trainMatrix, trainMatrix, vec3.fromValues(0.2, 0.2, 0.2));
+  this.train.draw(trainMatrix);
 };
 
 sg.Scene.prototype.tick = function(delta) {
